94c2dbeae107a4398990a29ddf7d62a2f2cdd988
louizeoosterlinck <43610118+louizeoosterlinck@users.noreply.github.com>
2020-03-31 13:37:03 +0200

    Add files via upload

---
+++ js/modal.js
@@ -0,0 +1,17 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var modalImg = document.getElementById("img01");
+function showModal(img){
+  modal.style.display = "block";
+  modalImg.src = img.src;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
